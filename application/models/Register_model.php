49033df6f74ab890583d81bb103af0fb8d5aa421
ujangaris <ujangaja@gmail.com>
2020-05-22 06:39:03 +0700

    Membuat Halaman Register dan merapihkan source code yang typo

--- application/models/Register_model.php
+++ application/models/Register_model.php
@@ -1,9 +1,10 @@
-<?php 
+<?php
 
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Register_model extends MY_Model {
+class Register_model extends MY_Model
+{
 
     protected $table = 'user';
 
@@ -20,39 +21,39 @@ class Register_model extends MY_Model {
 
     public function getValidationRules()
     {
-            $validationRules = [
+        $validationRules = [
             [
                 'field' => 'name',
                 'label' => 'Nama',
                 'rules' => 'trim|required',
-            
+
             ],
             [
                 'field'  => 'email',
                 'label'  => 'E-Mail',
-                'rules'  => 'required|valid_email|is_unique[user.email]',
+                'rules'  => 'trim|required|valid_email|is_unique[user.email]',
                 'errors' => [
                     'is_unique' => 'This %s already exists.'
                 ]
-            
+
             ],
             [
                 'field' => 'password',
                 'label' => 'Password',
                 'rules' => 'required|min_length[8]',
-            
+
             ],
             [
                 'field' => 'password_confirmation',
                 'label' => 'Konfirmasi Password',
                 'rules' => 'required|matches[password]',
-            
+
             ],
         ];
         return $validationRules;
     }
 
-    public function run()
+    public function run($input)
     {
         $data = [
             'name' => $input->name,
@@ -62,19 +63,18 @@ class Register_model extends MY_Model {
         ];
 
         $user = $this->create($data);
-        
+
         $sess_data = [
-            'id' =>$user,
-            'name' =>$data['name'],
-            'email' =>$data['email'],
-            'role' =>$data['role'],
-            'is_login' =>true,
+            'id' => $user,
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'role' => $data['role'],
+            'is_login' => true,
         ];
 
         $this->session->set_userdata($sess_data);
         return true;
     }
-
 }
 
 /* End of file Register_model.php */
