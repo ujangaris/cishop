cfd6162002bab929c258861302989492aee0ca0a
ujangaris <ujangaja@gmail.com>
2020-06-03 19:09:08 +0700

    Menampilkan Data Pengguna

---
+++ application/controllers/Profile.php
@@ -0,0 +1,32 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Profile extends MY_Controller
+{
+
+    private $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $is_login    = $this->session->userdata('is_login');
+        $this->id    = $this->session->userdata('id');
+
+        if (!$is_login) {
+            redirect(base_url());
+            return;
+        }
+    }
+
+    public function index()
+    {
+        $data['title']        = 'Profile';
+        $data['content']    = $this->profile->where('id', $this->id)->first();
+        $data['page']        = 'pages/profile/index';
+
+        return $this->view($data);
+    }
+}
+
+/* End of file Controllername.php */
