63a101731aae733d7ee87f5f0ad7f84bfeca3d9c
ujangaris <ujangaja@gmail.com>
2020-06-03 07:07:33 +0700

    Membuat Fungsi Ubah Pengguna

--- application/views/pages/user/index.php
+++ application/views/pages/user/index.php
@@ -45,10 +45,8 @@
                                     <td><?= $row->role ?></td>
                                     <td><?= $row->is_active ? 'Aktif' : 'Tidak Aktif' ?></td>
                                     <td>
-                                        <a href="#">
-                                            <button class="btn btn-sm">
-                                                <i class="fas fa-edit text-info"></i>
-                                            </button>
+                                        <a href="<?= base_url("user/edit/$row->id") ?>" class="btn btn-sm">
+                                            <i class="fas fa-edit text-info"></i>
                                         </a>
                                         <a href="#">
                                             <button class="btn btn-sm" type="submit" onclick="return confirm('Are you sure?');">
