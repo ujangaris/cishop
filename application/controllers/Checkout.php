9837c453bb944db41bad8c2b5bdad8125cf60a72
ujangaris <ujangaja@gmail.com>
2020-06-05 18:46:32 +0700

    Membuat Fungsi Tambah Order

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -42,6 +42,59 @@ class Checkout extends MY_Controller
 
         $this->view($data);
     }
+
+    public function create()
+    {
+        if (!$_POST) {
+            redirect(base_url('checkout'));
+        } else {
+            $input = (object) $this->input->post(null, true);
+        }
+
+        if (!$this->checkout->validate()) {
+            return $this->index($input);
+        }
+
+        $total    = $this->db->select_sum('subtotal')
+            ->where('id_user', $this->id)
+            ->get('cart')
+            ->row()
+            ->subtotal;
+
+        $data = [
+            'id_user'    => $this->id,
+            'date'        => date('Y-m-d'),
+            'invoice'    => $this->id . date('YmdHis'),
+            'total'        => $total,
+            'name'        => $input->name,
+            'address'    => $input->address,
+            'phone'        => $input->phone,
+            'status'    => 'waiting'
+        ];
+
+        if ($order = $this->checkout->create($data)) {
+            $cart = $this->db->where('id_user', $this->id)
+                ->get('cart')->result_array();
+            foreach ($cart as $row) {
+                $row['id_orders']    = $order;
+                unset($row['id'], $row['id_user']);
+                $this->db->insert('orders_detail', $row);
+            }
+
+            $this->db->delete('cart', ['id_user' => $this->id]);
+
+            $this->session->set_flashdata('success', 'Data berhasil disimpan.');
+
+            $data['title']        = 'Checkout Success';
+            $data['content']    = (object) $data;
+            $data['page']        = 'pages/checkout/success';
+
+            $this->view($data);
+        } else {
+            $this->session->set_flashdata('error', 'Oops! Terjadi suatu kesalahan.');
+            return $this->index($input);
+        }
+    }
 }
 
 /* End of file Checkout.php */
