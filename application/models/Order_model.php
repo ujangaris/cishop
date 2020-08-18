ee454c988a09984daace8d4ec599db5c687deb2f
ujangaris <ujangaja@gmail.com>
2020-08-18 17:33:08 +0700

    Menampilkan Data Detail Order

--- application/models/Order_model.php
+++ application/models/Order_model.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Order_model extends MY_Model {
+class Order_model extends MY_Model
+{
 
-    protected $table = 'orders';
-
+    public $table = 'orders';
 }
 
 /* End of file Order_model.php */
